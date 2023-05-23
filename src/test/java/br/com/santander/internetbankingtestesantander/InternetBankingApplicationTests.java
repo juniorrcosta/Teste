d08a33e1b5f3b6afeee84691f4884df687f0f69b
juniorrcosta <30703242+juniorrcosta@users.noreply.github.com>
2023-05-22 22:27:02 -0300

    Correção testes
    Correção testes

--- src/test/java/br/com/santander/internetbankingtestesantander/InternetBankingApplicationTests.java
+++ src/test/java/br/com/santander/internetbankingtestesantander/InternetBankingApplicationTests.java
@@ -1,9 +1,13 @@
 package br.com.santander.internetbankingtestesantander;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import br.com.santander.internetbanking.InternetBankingApplication;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = InternetBankingApplication.class)
 class InternetBankingApplicationTests {
 
 	@Test
